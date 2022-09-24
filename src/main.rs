7fa207aaec2011609b2f1f81963b367d3e81a3ec
Josue Morales <molinajosue92@hotmail.com>
2022-09-24 14:12:24 -0400

    add two functions

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     // if not correct, increment number_of_guesses
     // if correct, break out of inner loop, ask if they would like to play again
     // if play_again, increment number_of_games_played
-    'main_game: loop {
+    loop {
         // increase number_of_games_played
         number_of_games_played += 1;
         // reset number_of guesses
@@ -20,38 +20,48 @@ fn main() {
         // Get the random number
         let random_number: u32 = rand::thread_rng().gen_range(1..=100);
         // enter loop, get users guess
-        'guessing_loop: loop {
-            let mut guess = String::new();
-            print!("Please enter your guess >>> ");
-            io::stdout().flush().unwrap(); // if print is used, need to flush out stdout
-            io::stdin().read_line(&mut guess).expect("Error reading guess");
-            let guess = match guess.trim_end().parse::<u32>() {
-                Ok(num) => num,
-                Err(_) => {
-                    println!("Please enter a number!");
-                    continue;
-                }
-            };
-            number_of_guesses += 1;
-            match guess.cmp(&random_number) {
-                Ordering::Less => println!("To small! Try again."),
-                Ordering::Greater => println!("To big! Try again."),
-                Ordering::Equal => {
-                    println!("You guessed it in {number_of_guesses} guesses!");
-                    break 'guessing_loop;
-                }
-            }
-        }
-
-        print!("Would you like to play again? (y/n) >>> ");
-        io::stdout().flush().unwrap(); // if print is used, need to flush out stdout
-        let mut play_again = String::new();
-        io::stdin().read_line(&mut play_again).expect("Error reading play again");
-        match play_again.to_lowercase().chars().next().unwrap() {
-            'y' => continue 'main_game,
-            _ => break 'main_game
-        }
+        handle_user_guess(&mut number_of_guesses, random_number);
+        // Check to see if the user would like to play again
+        if handle_play_again() { continue; } else { break; }
     }
 
-    println!("Thanks for playing {name_of_user}! You played {number_of_games_played} times!");
+    println!("Thanks for playing {}! You played {number_of_games_played} times!", name_of_user.trim_end());
+}
+
+fn handle_user_guess(number_of_guesses: &mut u32, random_number: u32) {
+    loop {
+        let mut guess = String::new();
+        print!("Please enter your guess >>> ");
+        io::stdout().flush().unwrap(); // if print is used, need to flush out stdout
+        io::stdin().read_line(&mut guess).expect("Error reading guess");
+        let guess = match guess.trim_end().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            }
+        };
+        *number_of_guesses += 1;
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("To small! Try again."),
+            Ordering::Greater => println!("To big! Try again."),
+            Ordering::Equal => {
+                println!("You guessed it in {number_of_guesses} guesses!");
+                break;
+            }
+        }
+    }
+}
+
+fn handle_play_again() -> bool {
+    print!("Would you like to play again? (y/n) >>> ");
+    io::stdout().flush().unwrap(); // if print is used, need to flush out stdout
+
+    let mut play_again = String::new();
+    io::stdin().read_line(&mut play_again).expect("Error reading play again");
+
+    match play_again.to_lowercase().chars().next().unwrap() {
+        'y' => true,
+        _ => false
+    }
 }
